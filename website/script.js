433fdf359f1895583a43f95bbd9d549e27ddaafb
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-06-07 08:26:32 +0100

    removed the unused project

--- website/script.js
+++ website/script.js
@@ -6,10 +6,15 @@ const pageContent=document.querySelector(".container");
 
 sideBarButton.addEventListener("click",()=>{
 sideNav.classList.toggle("open")
+sideBarButton.classList.toggle("rotate")
 pageContent.classList.toggle("blured")
+closeSideBar.classList.remove("rotate")
+
 })
 
 closeSideBar.addEventListener("click",()=>{
    sideNav.classList.toggle("open")
+   closeSideBar.classList.toggle("rotate")
    pageContent.classList.toggle("blured")
+   sideBarButton.classList.remove("rotate")
 })
