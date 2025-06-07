a3d54cd627679310a0c3408b0cc2ea1629f0b576
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-06-07 08:01:16 +0100

    can finally say am done with the css of this ecommerce website

--- website/script.js
+++ website/script.js
@@ -1,5 +1,15 @@
-const button=document.querySelector(".brand-button text")
+const sideNav=document.querySelector(".sidebar")
+const closeSideBar=document.querySelector(".close-sidebar");
+const sideBarButton=document.querySelector(".navbutton");
+const pageContent=document.querySelector(".container");
 
-button.addEventListener("click",()=>{
-   alert("hello")
+
+sideBarButton.addEventListener("click",()=>{
+sideNav.classList.toggle("open")
+pageContent.classList.toggle("blured")
+})
+
+closeSideBar.addEventListener("click",()=>{
+   sideNav.classList.toggle("open")
+   pageContent.classList.toggle("blured")
 })
